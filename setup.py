24d3e6faa245d0cb3d3a41a6ddec7932597f864d
Ashish Dahal <ashishdahal1997@gmail.com>
2023-09-25 13:42:24 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+def get_install_requirements():
+    """
+    It reads the requirements.txt file and returns a list of all the requirements
+
+    :return: A list of all the requirements for the project.
+    """
+    with open("requirements.txt", "r", encoding="utf-8") as f:
+        reqs = [x.strip() for x in f.read().splitlines()]
+    reqs = [x for x in reqs if not x.startswith("#")]
+    return reqs
+
+
+setuptools.setup(
+    name="democracy-chatbot",
+    version='0.1.0',
+    author="Intelligent Systems Institute @ Novia",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=get_install_requirements(),
+)
