1216a9ce6032aecc1d08988d94916113b6af6d7f
Ashish Dahal <ashish.dahal@novia.fi>
2023-11-30 16:18:57 +0200

    move functions to utils

--- src/data_pipeline/meeting_metadata_extractor.py
+++ src/data_pipeline/meeting_metadata_extractor.py
@@ -1,28 +1,10 @@
-import fitz
 import json
 import os
 from openai import OpenAI
-import multiprocessing
 import pandas as pd
 import streamlit as st
-import sys
 from tqdm import tqdm
-
-
-def extract_data_with_llm(text, client, prompt):
-    print(text)
-    response = client.chat.completions.create(
-        model="gpt-4-1106-preview",
-        response_format={"type": "json_object"},
-        messages=[
-            {"role": "system",
-                "content": prompt},
-            {"role": "user", "content": text}
-        ]
-    )
-    print(response.choices[0].message.content)
-    sys.exit()
-    return response.choices[0].message.content.replace('```json', '').replace('```', '')
+from utils import process_html
 
 
 # def extract_data_with_llm(text, pdf_name, client, assistant_id):
@@ -62,41 +44,6 @@ def extract_data_with_llm(text, client, prompt):
 #     return messages.data[0].content[0].text.value.replace('```json', '').replace('```', '')
 
 
-def process_pdf(pdf_name, protocols_pdf_path, client, prompt):
-    '''
-    Process a single PDF file and return the PDF name and metadata as a tuple.
-
-    Args:
-        pdf_name (str): The filename of the PDF file to be processed.
-        protocols_pdf_path (str): The path to the directory containing the PDF file.
-        client (OpenAI): OpenAI client object.
-        prompt (str): The prompt to use for the LLM.
-
-    Returns:
-        tuple: A tuple containing the PDF filename and the extracted metadata.
-    '''
-
-    # Open and read the PDF file
-    with fitz.open(os.path.join(protocols_pdf_path, pdf_name)) as doc:
-        text = "".join(page.get_text(
-            "text", flags=~fitz.TEXT_PRESERVE_IMAGES) for page in doc)
-
-    json_response = extract_data_with_llm(text, client, prompt)
-
-    # Check if the response is valid JSON; retry if not
-    for _ in range(3):  # Retry up to 3 times
-        try:
-            response_data = json.loads(json_response)
-            return pdf_name, response_data
-        except json.JSONDecodeError:
-            print("Invalid JSON response. Retrying...")
-            json_response = extract_data_with_llm(
-                text, client, prompt)
-
-    print(f"Failed to get valid JSON response for {pdf_name} after retries.")
-    return pdf_name, "LLM Error!"
-
-
 def extract_meeting_metadata(df, meeting_titles_filter, protocols_html_path, client, prompt):
     """
     Extracts meeting metadata from a meeting documents.
@@ -118,7 +65,7 @@ def extract_meeting_metadata(df, meeting_titles_filter, protocols_html_path, cli
     results = []
     for filename in tqdm(filtered_df['doc_name']):
         filename = os.path.splitext(filename)[0] + '.html'
-        results.append(process_pdf(
+        results.append(process_html(
             filename, protocols_html_path, client, prompt))
 
     # add metadata to dataframe
@@ -148,22 +95,21 @@ def main():
 
     PROTOCOLS_HTML_PATH = os.getenv("PROTOCOLS_HTML_PATH")
     METADATA_FILE = os.getenv("METADATA_FILE")
-    MEETING_TITLES_FILTER = ['Beslutande', 'Sammanträdesuppgifter och deltagande',
-                             'Kokoustiedot ja osallistujat', 'Vln:Beslutande', 'Päättäjät']
-    MEETING_EXTRACTION_PROMPT_PATH = os.getenv(
-        "MEETING_EXTRACTION_PROMPT_PATH")
+    MEETING_TITLES_WITH_METADATA = ['Beslutande', 'Sammanträdesuppgifter och deltagande',
+                                    'Kokoustiedot ja osallistujat', 'Vln:Beslutande', 'Päättäjät']
+    METADATA_EXTRACTION_PROMPT_PATH = os.getenv(
+        "METADATA_EXTRACTION_PROMPT_PATH")
 
     # Initialize the OpenAI client and assistant_id
     client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-    assistant_id = st.secrets["OPENAI_ASSISTANT_ID"]
 
     # read the prompt text
-    with open(MEETING_EXTRACTION_PROMPT_PATH, 'r') as file:
+    with open(METADATA_EXTRACTION_PROMPT_PATH, 'r') as file:
         prompt = file.read()
 
     df = pd.read_csv(METADATA_FILE)
     df = extract_meeting_metadata(
-        df, MEETING_TITLES_FILTER, PROTOCOLS_HTML_PATH, client, prompt)
+        df, MEETING_TITLES_WITH_METADATA, PROTOCOLS_HTML_PATH, client, prompt)
 
     # Save the updated DataFrame
     df.to_csv(METADATA_FILE)
---
+++ src/data_pipeline/utils.py
@@ -0,0 +1,62 @@
+import json
+import os
+from openai import OpenAI
+
+
+def extract_data_with_llm(text, client, prompt):
+    '''Extract data from a HTML file using the LLM.
+
+    Args:
+        text (str): The extracted text from the PDF file.
+        client (OpenAI): OpenAI client object.
+        prompt (str): The prompt to use for the LLM.
+
+    Returns:
+        str: The extracted data as a JSON string.
+    '''
+    print(text)
+    response = client.chat.completions.create(
+        model="gpt-4-1106-preview",
+        response_format={"type": "json_object"},
+        messages=[
+            {"role": "system",
+                "content": prompt},
+            {"role": "user", "content": text}
+        ]
+    )
+    print(response.choices[0].message.content)
+    return response.choices[0].message.content.replace('```json', '').replace('```', '')
+
+
+def process_html(filename, filepath, client, prompt):
+    '''
+    Process a single HTML file and return the file name and metadata as a tuple.
+
+    Args:
+        filename (str): The filename (without extension) to be processed.
+        filepath (str): The path to the directory containing the HTML file.
+        client (OpenAI): OpenAI client object.
+        prompt (str): The prompt to use for the LLM.
+
+    Returns:
+        tuple: A tuple containing the PDF filename and the extracted metadata.
+    '''
+
+    # Open and read the html file
+    with open(os.path.join(filepath, filename)) as doc:
+        text = doc.read()
+
+    json_response = extract_data_with_llm(text, client, prompt)
+
+    # Check if the response is valid JSON; retry if not
+    for _ in range(3):  # Retry up to 3 times
+        try:
+            response_data = json.loads(json_response)
+            return filename + ".pdf", response_data
+        except json.JSONDecodeError:
+            print("Invalid JSON response. Retrying...")
+            json_response = extract_data_with_llm(
+                text, client, prompt)
+
+    print(f"Failed to get valid JSON response for {filename} after retries.")
+    return filename + ".pdf", "LLM Error!"
