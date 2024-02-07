df1eab8b939cbede011235eecce85ba2df20d9ce
Ashish Dahal <ashishdahal1997@gmail.com>
2024-02-07 14:30:32 +0200

    Update file converter function and import statements

--- src/data_pipeline/__init__.py
+++ src/data_pipeline/__init__.py
@@ -1,6 +1,6 @@
 from .website_scraper import main as scrape_website
 from .document_downloader import main as download_documents
-from .pdf_coverter import main as convert_pdfs
+from .file_coverter import main as convert_files
 from .meeting_metadata_extractor import main as extract_meeting_metadata
 from .meeting_agenda_extractor import main as extract_meeting_agenda
-from .utils import get_file_path
+from .utils import get_documents_dataframe, convert_file_path
