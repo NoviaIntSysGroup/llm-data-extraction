b03bdd587fc3b8ed92d91be00a3762b3febb4e36
Ashish Dahal <ashishdahal1997@gmail.com>
2024-02-12 16:48:28 +0200

    Import all functions from utils module in data_pipeline package

--- src/data_pipeline/__init__.py
+++ src/data_pipeline/__init__.py
@@ -2,4 +2,4 @@ from .website_scraper import main as scrape_website
 from .document_downloader import main as download_documents
 from .file_coverter import main as convert_files
 from .meeting_data_extractor import extract_meeting_data
-from .utils import get_documents_dataframe, convert_file_path
+from .utils import *
