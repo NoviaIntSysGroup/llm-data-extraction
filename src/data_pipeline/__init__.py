1b1249dab5341b65219d72b21ac13ac9116ce7ea
ashish-dahal <ashishdahal1997@gmail.com>
2024-12-19 11:56:31 +0200

    fix import statements in data pipeline to correct module names and include additional functionalities

--- src/data_pipeline/__init__.py
+++ src/data_pipeline/__init__.py
@@ -1,6 +1,8 @@
 from .website_scraper import main as scrape_website
 from .document_downloader import main as download_documents
-from .file_coverter import main as convert_files
-from .meeting_data_extractor import extract_meeting_data
+from .file_converter import main as convert_files
+from .meeting_data_extractor import *
 from .kg_creator import create_knowledge_graph
+from .evaluations import *
+from .result_visualization import *
 from .utils import *
